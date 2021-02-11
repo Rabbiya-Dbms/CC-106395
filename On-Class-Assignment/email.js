e7bc3bbf15ff09f2eb7fc9fb85e2ad07c020c399
ayeshaaman <57321450+ayeshaaman@users.noreply.github.com>
2021-02-11 20:40:06 +0500

    Update email.js

--- On-Class-Assignment/email.js
+++ On-Class-Assignment/email.js
@@ -21,3 +21,31 @@ function emailValid(input) {
   }
 
 }
+
+
+
+
+
+//js email regrex
+
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+
+
+//email regrex javascript
+/* Answer to: "email regex javascript" */
+
+ValidateEmail("icecream123@yahoo.com"); // Must be a string
+
+function ValidateEmail(email) {
+	var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String(email).toLowerCase());
+	if (email.match(emailformat)) {
+    	alert("Nice Email!")
+      	return true;
+    };
+    alert("That's not an email?!")
+    return (false);
+};
